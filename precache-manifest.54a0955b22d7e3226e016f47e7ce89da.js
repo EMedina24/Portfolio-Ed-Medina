eb15602340a4e474d186ad088fe811871a90da15
ed <edgardo.medina@ironman.com>
2020-10-09 20:17:01 -0400

    Updates

---
+++ precache-manifest.54a0955b22d7e3226e016f47e7ce89da.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95addc6de1dc91735e454560ae7ff55",
+    "url": "/Portfolio-Ed-Medina/index.html"
+  },
+  {
+    "revision": "aaab6cec9936299046c8",
+    "url": "/Portfolio-Ed-Medina/static/css/2.9df4d845.chunk.css"
+  },
+  {
+    "revision": "7744045111a915f049a5",
+    "url": "/Portfolio-Ed-Medina/static/css/main.04c8234a.chunk.css"
+  },
+  {
+    "revision": "aaab6cec9936299046c8",
+    "url": "/Portfolio-Ed-Medina/static/js/2.bff098fc.chunk.js"
+  },
+  {
+    "revision": "d2b77ccf1430e33b4437ca054c773458",
+    "url": "/Portfolio-Ed-Medina/static/js/2.bff098fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7744045111a915f049a5",
+    "url": "/Portfolio-Ed-Medina/static/js/main.3c98a7da.chunk.js"
+  },
+  {
+    "revision": "5f4d7af2d4ae99b1cb74",
+    "url": "/Portfolio-Ed-Medina/static/js/runtime-main.b7edad71.js"
+  },
+  {
+    "revision": "7c33af2f1a5b73e5d6884ea1846a054d",
+    "url": "/Portfolio-Ed-Medina/static/media/boost-your-post-for-increasing-sales.7c33af2f.md"
+  },
+  {
+    "revision": "9f553f26846bbd2553a9ed2e3a604ed6",
+    "url": "/Portfolio-Ed-Medina/static/media/difference-between-gatsbyjs-and-nextjs.9f553f26.md"
+  },
+  {
+    "revision": "8cc16894642414291f3700ac5396c1ae",
+    "url": "/Portfolio-Ed-Medina/static/media/future-of-javascript.8cc16894.md"
+  },
+  {
+    "revision": "94fbd3842288f7042fb36a2b025fb7a5",
+    "url": "/Portfolio-Ed-Medina/static/media/how-to-choose-javascript-framework-for-project.94fbd384.md"
+  },
+  {
+    "revision": "6566a29212d29ce3455d349471c4c4c3",
+    "url": "/Portfolio-Ed-Medina/static/media/how-to-get-10k-instagram-followers.6566a292.md"
+  },
+  {
+    "revision": "368cc11c0fc1f5868a2461a538b6dd81",
+    "url": "/Portfolio-Ed-Medina/static/media/installing-nodejs-on-your-device.368cc11c.md"
+  },
+  {
+    "revision": "c7fe48e63067433331f84ec94e2e49fc",
+    "url": "/Portfolio-Ed-Medina/static/media/markdown-html-supported-blog.c7fe48e6.md"
+  },
+  {
+    "revision": "d4881e738d4008ebb0ad0ed9e6587da5",
+    "url": "/Portfolio-Ed-Medina/static/media/popular-javascript-library-in-2020.d4881e73.md"
+  },
+  {
+    "revision": "d490be272abdf01ca26f90685cec9b99",
+    "url": "/Portfolio-Ed-Medina/static/media/programmers-must-read-books.d490be27.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Portfolio-Ed-Medina/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Portfolio-Ed-Medina/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Portfolio-Ed-Medina/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Portfolio-Ed-Medina/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c0c0a9c9cc01fca9adedff7998f6ac05",
+    "url": "/Portfolio-Ed-Medina/static/media/think-out-of-the-box.c0c0a9c9.md"
+  },
+  {
+    "revision": "ea70d882cb4362f1ca2c8db077b78794",
+    "url": "/Portfolio-Ed-Medina/static/media/time-to-use-latest-technology-for-creating-a-website.ea70d882.md"
+  },
+  {
+    "revision": "8e1efcf25b01edbf3d60df3f09646750",
+    "url": "/Portfolio-Ed-Medina/static/media/trending-designs-in-2020.8e1efcf2.md"
+  },
+  {
+    "revision": "c7973cd5f0da590119aa22cd3147a84f",
+    "url": "/Portfolio-Ed-Medina/static/media/uiux-design-starter-with-adobe-xd.c7973cd5.md"
+  },
+  {
+    "revision": "a1c7c37f05e57483b9ecb3edbdf5e5a3",
+    "url": "/Portfolio-Ed-Medina/static/media/web-automation-with-python-language.a1c7c37f.md"
+  },
+  {
+    "revision": "40686a4e31c7336491e3abce057010eb",
+    "url": "/Portfolio-Ed-Medina/static/media/what-nodejs-can-do.40686a4e.md"
+  }
+]);
